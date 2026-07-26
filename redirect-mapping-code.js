64e43e5b0950600384c3ebe44d78bd75b36de2c4
ravikumarmalhotra <ravikumarmalhotra343@gmail.com>
2026-07-26 09:05:42 +0530

    Update redirect-mapping-code.js

--- redirect-mapping-code.js
+++ redirect-mapping-code.js
@@ -1,1 +1,17 @@
-
+const qrMapping = [
+  { sn: 1, code1: "200010101", code2: "200HB01" },
+  { sn: 2, code1: "200010102", code2: "200HB02" },
+  { sn: 3, code1: "200010103", code2: "200HB03" },
+  { sn: 4, code1: "200010104", code2: "200HB04" },
+  { sn: 5, code1: "200010105", code2: "200HB05" },
+  { sn: 6, code1: "200010106", code2: "200HB06" },
+  { sn: 7, code1: "200010107", code2: "200HB07" },
+  { sn: 8, code1: "200010108", code2: "200HB08" },
+  { sn: 9, code1: "200010109", code2: "200HB09" },
+  { sn: 10, code1: "200010110", code2: "200HB10" },
+  { sn: 11, code1: "200010111", code2: "200HB11" },
+  { sn: 12, code1: "200010121", code2: "200BRP01" },
+  { sn: 13, code1: "201010101", code2: "201HB01" },
+  { sn: 14, code1: "201010102", code2: "201HB02" }
+  // ... 1000 tak continue karo
+];
